10509dc52e080dda3fb95b2702d5bca34e95f526
Dave Prukop <dave@unitedweego.com>
2011-10-21 18:16:52 -0700

    more feed stuff

--- public/js/FeedMessage.js
+++ public/js/FeedMessage.js
@@ -19,3 +19,41 @@ FeedMessage.prototype.populateWithXML = function(xml) {
     this.messageSentTimestamp = $(xml).attr("timestamp");
     this.message = $(xml).find("message").text();
 }
+
+FeedMessage.prototype.getDateFromString = function(dateStr) {
+	var testDate = new Date(2000,12,1);
+	var monthCorrection = 0;
+	if (testDate.getFullYear() != 2000) {
+		monthCorrection = 1;
+	}
+	var a=dateStr.split(" ");
+	d=a[0].split("-");
+	t=a[1].split(":");
+	return new Date(d[0],parseInt(d[1],10)-monthCorrection,d[2],t[0],t[1],t[2]);
+}
+
+FeedMessage.prototype.getFriendlyTimestamp = function() {
+	var now = new Date();
+	now.setMilliseconds(0);
+	now.setSeconds(0);
+	var adjustedDate = new Date(now.getTime() + (now.getTimezoneOffset() * 60 * 1000));
+	var sent = this.getDateFromString(this.messageSentTimestamp);
+	sent.setSeconds(0);
+	var minutes = (adjustedDate.getTime() - sent.getTime()) / (1000 * 60);
+	if (minutes < 2) return "Just Now";
+	var hours = Math.floor(minutes/60);
+	var days = 0;
+	if (hours > 0) days = Math.floor(hours/24);
+	var output = '';
+	if (days > 0) {
+		output = days +" day";
+		if (days > 1) output += "s";
+	} else if (hours > 0) {
+		output = hours +" hour";
+		if (hours > 1) output += "s";
+	} else {
+		output = minutes +" minute";
+		if (minutes > 1) output += "s";
+	}
+	return output;
+}
--- public/js/JQ_ViewMessages.js
+++ public/js/JQ_ViewMessages.js
@@ -23,19 +23,28 @@
 						setViewSize();
 					});
 					
-					setScroll();
-					
-					setViewSize();
-					
 					setEvents();
 					
 					setUpUI();
+					
+					setViewSize();
+					
+					setScroll();
+					
+					setUpMessageInput();
+					
+					resetScroll();
 				};
 				
 				update();
 				
 				function setViewSize() {
-					$this.find('.content').css('height',document.documentElement.clientHeight - resizeOffset);
+					var messageInputHeight = ($this.find('.messageInput')) ? $this.find('.messageInput').height() : 0;
+					$this.find('.contentContainer').css('height',document.documentElement.clientHeight - resizeOffset - messageInputHeight);
+					//$this.find('.content').css('height', $this.find('.content').find('.messageList').height());
+					//$this.find('.content').find('.messageList').css('min-height', $this.find('.contentContainer').height());
+					//alert($this.find('.content').find('.messageList').height());
+					$this.find('.messageInput').find('TEXTAREA').css('width',document.documentElement.clientWidth - 18);
 					var titleWidth = $this.find('.feedTitle').width();
 					var left = (document.documentElement.clientWidth - titleWidth) / 2;
 					$this.find('.feedTitle').css('margin-left', left+'px');
@@ -44,14 +53,20 @@
 				function setScroll() {
 					if (!!('ontouchstart' in window)) {
 						$this.find('.content:first-child').touchScroll();
-						$this.find('.content').unbind('touchScroll');
+						$this.find('.content:first-child').unbind('touchScroll');
 						$this.find('.content:first-child').bind('touchScroll', function(){
 							determineRefresh();
 						});
-						$this.find('.content').unbind('touchEnd');
+						$this.find('.content:first-child').unbind('touchEnd');
 						$this.find('.content:first-child').bind('touchEnd', function(){
 							doRefresh();
 						});
+					}
+				}
+				
+				function resetScroll() {
+					if (!!('ontouchstart' in window)) {
+						$this.find('.content:first-child').touchScroll('setPosition', 0);
 					}
 				}
 				
@@ -107,28 +122,82 @@
 					$this.find('.closeButton').click(function() {
 						ViewController.getInstance().hideMessages();
 					});
+					$this.find('.sendButton').unbind('click');
+					$this.find('.sendButton').click(function() {
+						sendMessage();
+					});
+				}
+				
+				function sendMessage() {
+					var text = $this.find('.messageInput').find('TEXTAREA').val();
+					var woWhiteSpace = text.replace(/ /g,'');
+					if (woWhiteSpace.length > 0) {
+						var now = new Date();
+						var url = domain + "/mod.feedmessage.php";
+						$.get(url, {registeredId:ruid, eventId:o.event.eventId, message:escape(text)}, function(data) { //, timestamp:now.format('yyyy-m-dd h:MM:ss')
+							handleSendMessageResponse(data);
+						});
+					}
+				}
+				
+				function handleSendMessageResponse(data) {
+					Model.getInstance().populateEventsWithXML(data);
+					$this.find('.messageInput').find('TEXTAREA').val('');
+					updateChars();
+					setUpUI();
+					setViewSize();
 				}
 				
 				function setUpUI() {
 					$this.find('.content').html('');
-					//$this.find('.content').append('<div class="refreshHeader dark"><div class="refreshArrow"></div><div class="refreshContent">Pull down to refresh...</div></div>');
+					$this.find('.content').append('<div class="refreshHeader dark"><div class="refreshArrow"></div><div class="refreshContent">Pull down to refresh...</div></div>');
 					$this.find('.content').append('<ul class="messageList">');
 					for (var i=o.event.allMessages.length-1; i>=0; i--) {
 						var m = o.event.allMessages[i];
 						var sender;
 						var imageTag;
 						var message;
+						var status;
 						if (m.type == "decided") {
-							imageTag = '<img class="decided" src="/assets/images/POIs_decided_default_sm.png" />';
-							sender = '<span class="sender">Weego</span><br />';
-							message = '<span class="message">"'+ o.event.getWinningLocation().name +'" is where we are going!</span>';
+							imageTag = 	'<img class="decided" src="/assets/images/POIs_decided_default_sm.png" />';
+							sender = 	'<span class="sender">Weego</span><br />';
+							message = 	'<span class="message">"'+ unescape(o.event.getWinningLocation().name) +'" is where we are going!</span>';
+							status = 	'<div class="statusBlock">';
+							if (!m.userReadMessage) status += '<div class="newIcon"><img src="/assets/images/icon_feed_new.png" /></div>';
+							status += 		'<div class="status">'+ m.getFriendlyTimestamp() +'</div>';
+							status += 	'</div>';
+						} else if (m.type == "timesuggestion") {
+							var p = o.event.getParticipantById(m.senderId);
+							imageTag = 	'<img class="avatar" src="'+ p.avatarURL +'" />';
+							sender = 	'<span class="sender">'+ unescape(p.getFullName()) +'</span><br />';
+							var newTime = m.getDateFromString(m.message);
+							var adjustedTime = new Date(newTime.getTime() - (newTime.getTimezoneOffset() * 60 * 1000));
+							message = 	'<span class="message">Suggests '+ adjustedTime.format("mmmm dd 'at' h:MM TT") +' as an alternate event time.</span>';
+							status = 	'<div class="statusBlock">';
+							if (!m.userReadMessage) status += '<div class="newIcon"><img src="/assets/images/icon_feed_new.png" /></div>';
+							status += 		'<div class="status">'+ m.getFriendlyTimestamp() +'</div>';
+							status += 	'</div>';
 						} else {
 							var p = o.event.getParticipantById(m.senderId);
-							imageTag = '<img src="'+ p.avatarURL +'" />';
-							sender = '<span class="sender">'+ p.getFullName() +'</span><br />';
-							message = '<span class="message">'+ m.message +'</span>';
+							imageTag = 	'<img class="avatar" src="'+ p.avatarURL +'" />';
+							sender = 	'<span class="sender">'+ unescape(p.getFullName()) +'</span><br />';
+							message = 	'<span class="message">'+ unescape(m.message) +'</span>';
+							status = 	'<div class="statusBlock">';
+							if (!m.userReadMessage) status += '<div class="newIcon"><img src="/assets/images/icon_feed_new.png" /></div>';
+							status += 		'<div class="status">'+ m.getFriendlyTimestamp() +'</div>';
+							if (m.type == "locationadd") {
+								status +=	'<div class="statusIcon"><img src="/assets/images/icon_feed_places.png" /></div>';
+							} else if (m.type == "timechange") {
+								status +=	'<div class="statusIcon"><img src="/assets/images/icon_feed_time_01.png" /></div>';
+							} else if (m.type == "invite") {
+								status +=	'<div class="statusIcon"><img src="/assets/images/icon_feed_people.png" /></div>';
+							} else if (m.type == "checkin") {
+								status +=	'<div class="statusIcon"><img src="/assets/images/icon_feed_checkin.png" /></div>';
+							}
+							status += 	'</div>';
 						}
 						var html = 	'<li>';
+						html +=			status;
 						html +=			imageTag;
 						html +=			sender;
 						html +=			message;
@@ -136,6 +205,24 @@
 						$this.find('.content').find('.messageList').append(html);
 					}
 				}
+				
+				function setUpMessageInput() {
+					$this.find('.messageInput').find('TEXTAREA').val('');
+					updateChars();
+					$this.find('.messageInput').find('TEXTAREA').keyup(function(){
+						updateChars();
+					});
+				}
+				
+				function updateChars() {
+					var text = $this.find('.messageInput').find('TEXTAREA').val();
+					var charsAmt = 140;
+					//if (text) {
+						charsAmt -= text.length;
+					//}
+					$this.find('.chars').html(charsAmt);
+				}
+				
 			});
 		},		
 	};
